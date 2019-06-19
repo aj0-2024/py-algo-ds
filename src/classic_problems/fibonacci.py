267df100be61a008bde35f9639d30fe137c74d66
Anirudh Janga <j.anirudh@icloud.com>
2019-06-18 23:27:35 -0500

    Implementing iteration and generator functions for fib and tests

--- src/classic_problems/fibonacci.py
+++ src/classic_problems/fibonacci.py
@@ -5,7 +5,7 @@ two preceding numbers
 
 The series is a common pattern observed in nature, ex: Arrangement of sunflower seeds in the flower
 """
-from typing import Dict
+from typing import Dict, Generator
 from functools import lru_cache
 
 # custom exception to not compute fib series for less than 0
@@ -34,30 +34,46 @@ fib_table: Dict[int, int] = {0: 0, 1: 1}
 def fib_from_table(n: int) -> int:
     """Calculating fib series using memoization"""
     if n in fib_table:
-        return n
+        return fib_table[n]
     else:
         fib_table[n] = fib_from_table(n-1) + fib_from_table(n-2)
         return fib_table[n]
 
 
 @lru_cache(maxsize=None)
-def fib_memozation(n: int) -> int:
+def fib_memoization(n: int) -> int:
     """Calculate fib series using builtin python memoization function"""
     if n in {0, 1}:
         return n
     elif n < 0:
         raise NegativeNumberError
     else:
-        return fib_memozation(n-1) + fib_memozation(n-2)
+        return fib_memoization(n-1) + fib_memoization(n-2)
 
 
 def fib_iteration(n: int) -> int:
     """Calculate fib using iterative method
     Any problem that can be solved with recursion can be solved with iterative methods
     """
-    pass
+    if n in {0, 1}:
+        return n
+    elif n < 0:
+        raise NegativeNumberError
+    else:
+        prev, curr = 0, 1
+        for _ in range(1, n):
+            prev, curr = curr, curr + prev
+        return curr
 
 
-def fib_generator(n: int) -> int:
+def fib_generator(n: int) -> Generator(int, None, None):
     """Calculate fib using python generators"""
-    pass
+    if n in {0, 1}:
+        yield n
+    elif n < 0:
+        raise NegativeNumberError
+    else:
+        prev, curr = 0, 1
+        for _ in range(1, n):
+            prev, curr = prev, prev + curr
+            yield curr
--- src/classic_problems/test_fibonacci.py
+++ src/classic_problems/test_fibonacci.py
@@ -1,12 +1,21 @@
-from .fibonacci import (fib_recursion)
+from .fibonacci import (fib_recursion, fib_from_table,
+                        fib_memoization, fib_generator, fib_iteration)
+import pytest
+
+questions_answers = [
+    (0, 0),
+    (1, 1),
+    (54, 86267571272),
+    (100, 354224848179261915075),
+    (200, 280571172992510140037611932413038677189525)
+]
 
 
-class TestFib(object):
-    def test_0(self):
-        assert fib_recursion(0) == 0
-
-    def test_1(self):
-        assert fib_recursion(1) == 1
-
-    def test_2(self):
-        assert fib_recursion(2) == 1
+@pytest.mark.parametrize("test_input,expected", questions_answers)
+def test_fib(test_input, expected):
+    for func in [
+        fib_from_table,
+        fib_memoization,
+        fib_iteration
+    ]:
+        assert func(test_input) == expected
