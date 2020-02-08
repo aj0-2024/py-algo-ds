6de8771f46bd034f13bc6a7238bcd10462308d58
Anirudh Janga <j.anirudh@icloud.com>
2020-02-08 14:23:40 -0600

    do not name variables after the ds that is used

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -9,51 +9,51 @@ Bubble sort is a basic sorting algorithm that sorts elements of a list in-place
 import unittest
 
 
-def bubble_sort(input_list):
+def bubble_sort(input_data):
     """Implementation of the bubble sort algorithm, employs an enhancement where the inner loops
     keep track of the last sorted element as described in the video below.
 
     (https://www.youtube.com/watch?v=6Gv8vg0kcHc)
 
-    The input_list is sorted in-place, this is dangerous to do
+    The input_data is sorted in-place, this is dangerous to do
     """
 
     is_sorted = False
-    last_unsorted_index = len(input_list) - 1
+    last_unsorted_index = len(input_data) - 1
 
     while not is_sorted:
         is_sorted = True
 
         for curr_index in range(0, last_unsorted_index):
-            curr_element = input_list[curr_index]
+            curr_element = input_data[curr_index]
 
             # be sure to not go out of bounds in the loop since we are accessing
             # the next element, happens at the edge of the list
             next_index = curr_index + 1
-            next_element = input_list[next_index]
+            next_element = input_data[next_index]
 
             # swap the elements that are not in order
             if curr_element > next_element:
-                input_list[curr_index], input_list[next_index] =\
-                    input_list[next_index], input_list[curr_index]
+                input_data[curr_index], input_data[next_index] =\
+                    input_data[next_index], input_data[curr_index]
                 is_sorted = False
 
         # as we loop everytime, the last elements of the list will be in order
         # this is explained in the video in the docstring above
         last_unsorted_index -= 1
 
-    return input_list
+    return input_data
 
 
 class TestBubbleSort(unittest.TestCase):
 
     def test_case_simple(self):
-        input_list = [5, 3, 2, 3, 4]
+        input_data = [5, 3, 2, 3, 4]
         expected_output = [2, 3, 3, 4, 5]
 
-        bubble_sort(input_list)
+        bubble_sort(input_data)
 
-        self.assertEqual(input_list, expected_output)
+        self.assertEqual(input_data, expected_output)
 
 
 if __name__ == "__main__":
