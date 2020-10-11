67dfe2d1ff114ca76603159a4dbdb22ab2990048
Anirudh Janga <j.anirudh@icloud.com>
2020-10-11 07:21:34 -0500

    Adding maximum subarray algorithm

---
+++ algorithms/maximum_subarray.py
@@ -0,0 +1,14 @@
+"""
+Notes:
+    - Divide and conquer method.
+    - Recursive & Iterative.
+
+Applications:
+    - Stock problem where maximum profit has to be calculated. 
+"""
+
+def recursive_find_max_subarray():
+    pass
+
+def iterative_find_max_subarray():
+    pass
