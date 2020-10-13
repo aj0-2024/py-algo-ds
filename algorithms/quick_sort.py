9537964a3fa08eeedd81aab16a33c1265cadf8fe
Anirudh Janga <j.anirudh@icloud.com>
2020-10-13 07:59:22 -0500

    Quick sort tests

--- algorithms/quick_sort.py
+++ algorithms/quick_sort.py
@@ -1,17 +1,22 @@
 """
-Quick sort is a simple algorithm that runs on average O(nlogn)
-1. Divide collection into roughly two equal parts using a pivot
-2. Elements larger than pivot move to the right of pivot and elements smaller than pivot
-move to the left.
-3. Repeat until the whole array is sorted
+Notes:
+    - Quick sort is a simple algorithm that runs on average O(nlogn)
+        1. Divide collection into roughly two equal parts using a pivot
+        2. Elements larger than pivot move to the right of pivot and elements smaller than pivot
+            move to the left.
+        3. Repeat until the whole sequence is sorted
 
-https://stackabuse.com/quicksort-in-python/
+    - Unstable algorithm (Does not guarantee the positions of items stay the same.
+
+References:
+    - https://stackabuse.com/quicksort-in-python/
 """
+import unittest
 
 def partition(sequence, start, end):
     """Divide the list into two and move elements around pivot"""
 
-    pivot = sequence[end]
+    pivot = sequence[start]
     low = start + 1
     high = end
 
@@ -27,20 +32,36 @@ def partition(sequence, start, end):
         else:
             break
 
-    array[start], array[end] = array[end], array[start]
+    # since the pivot is at the start, swap [start] & [high]
+    # switch this if you want to change the pivot
+    sequence[start], sequence[high] = sequence[high], sequence[start]
 
     return high
 
-def quick_sort(sequence, start, end, comparator):
+def quick_sort(sequence, start, end):
     """Perform quick sort on the sequence in place"""
     
     if start >= end:
         return sequence
 
-    high = partition(sequence, start, end, comparato)
-    quick_sort(sequence, start, high - 1, comparator)
-    quick_sort(sequence, high + 1, end, comparator)
+    mid = partition(sequence, start, end)
+    quick_sort(sequence, start, mid - 1)
+    quick_sort(sequence, mid + 1, end)
 
     return sequence
+
+class TestQuickSort(unittest.TestCase):
+
+    def test_empty(self):
+        sequence = []
+        self.assertEqual(quick_sort(sequence, 0, 0), [])
+
+    def test_basic_case(self):
+        sequence = [ 5, 4, 3, 2, 1 ]
+        self.assertEqual(quick_sort(sequence, 0, len(sequence) - 1), [ 1, 2, 3, 4, 5])
+
+
+if __name__ == "__main__":
+    unittest.main()
 
 
