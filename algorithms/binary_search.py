e424f972aa41ce575e223761974d5bb0a12edb4f
Anirudh Janga <j.anirudh@icloud.com>
2020-10-14 08:16:15 -0500

    Binary search test cases and debuggin max subarray

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -3,6 +3,7 @@ Binary search is a very important algorithm which enables the user to search for
 by incrementally chopping the dataset in half. The search happens in O(logn) time and O(1) space.
 The requirement is that the problem set has to be in a sorted order.
 """
+import unittest
 from typing import List
 
 
@@ -32,3 +33,26 @@ def binary_search(input_array: List[int], target: int):
             return mid_pointer
 
     return None
+
+class TestBinarySearch(unittest.TestCase):
+
+    def test_empty_seq(self):
+        seq: List[int] = []
+        target: int = 4
+
+        self.assertEqual(binary_search(seq, target), None)
+
+    def test_seq_with_one_item(self):
+        seq: List[int] = [ 1 ]
+
+        self.assertEqual(binary_search(seq, 4), None)
+        self.assertEqual(binary_search(seq, 1), 0)
+
+    def test_simple_seq(self):
+        seq: List[int] = [ 1, 2, 3, 4, 5]
+        target: int = 3
+
+        self.assertEqual(binary_search(seq, target), 2)
+
+if __name__ == "__main__":
+    unittest.main()
--- algorithms/maximum_subarray.py
+++ algorithms/maximum_subarray.py
@@ -11,7 +11,8 @@ TODO:
     - The implementations are incomplete.
 """
 import unittest
-from typing import List 
+from typing import List
+
 
 def brute_force_max_subarray(sequence: List[int], low: int, high: int):
     """Brute force implementation of the maximum subarray problem"""
@@ -31,32 +32,41 @@ def brute_force_max_subarray(sequence: List[int], low: int, high: int):
 
     return max_sum
 
+
 def find_max_cross_subarray(sequence: List[int], low: int, mid: int, high: int):
     """Find max with a crossover point at mid"""
 
     # calculate the max_left, left_sum
     left_sum = float("-inf")
     curr_sum = 0
+
     # TODO: None check
     max_left = None
+
     for left_index in range(mid, low):
         curr_sum += sequence[left_index]
+        print(curr_sum, left_sum)
         if curr_sum > left_sum:
             left_sum = curr_sum
             max_left = left_index
 
-    # cacluclate the right sum
+    # calculate the right sum
     right_sum = float("-inf")
     curr_sum = 0
+
     # TODO: None check
     max_right = None
+
     for right_index in range(mid+1, high):
         curr_sum += sequence[right_index]
         if curr_sum > right_sum:
             right_sum = curr_sum
             max_right = right_index
 
+    print(max_left, max_right, left_sum + right_sum)
+
     return (max_left, max_right, left_sum + right_sum)
+
 
 def recursive_find_max_subarray(sequence: List[int], low: int, high: int) -> (int, int, int):
     """Using recursive method to solve the maximum subarray"""
@@ -67,9 +77,12 @@ def recursive_find_max_subarray(sequence: List[int], low: int, high: int) -> (in
 
     mid = (low + high) // 2
 
-    left_low, left_high, left_sum = recursive_find_max_subarray(sequence, low, mid)
-    right_low, right_high, right_sum = recursive_find_max_subarray(sequence, mid + 1, high)
-    cross_low, cross_high, cross_sum = find_max_cross_subarray(sequence, low, mid, high)
+    left_low, left_high, left_sum = recursive_find_max_subarray(
+        sequence, low, mid)
+    right_low, right_high, right_sum = recursive_find_max_subarray(
+        sequence, mid + 1, high)
+    cross_low, cross_high, cross_sum = find_max_cross_subarray(
+        sequence, low, mid, high)
 
     if left_sum >= right_sum and left_sum >= cross_sum:
         return (left_low, left_high, left_sum)
@@ -77,22 +90,24 @@ def recursive_find_max_subarray(sequence: List[int], low: int, high: int) -> (in
         return (right_low, right_high, right_sum)
     else:
         return (cross_low, cross_high, cross_sum)
-    
+
 
 def iterative_find_max_subarray():
     pass
 
+
 class TestMaxSubarray(unittest.TestCase):
-    
+
     def test_max_subarray(self):
-        input = [ 2, 3, 4, -2, -1 ]
+        input = [10, 11, 7, 10, 6]
         low, high, output = recursive_find_max_subarray(input, 0, 4)
 
         print(low, high, output)
-        
-        self.assertEqual(low, 0)
-        self.assertEqual(high, 2)
-        self.assertEqual(ouput, 9)
+
+        self.assertEqual(low, 2)
+        self.assertEqual(high, 3)
+        self.assertEqual(output, 3)
+
 
 if __name__ == "__main__":
     unittest.main()
